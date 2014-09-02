8f064f7d52e2806c909129dfff2b9009d8cc1b9b
gvolpe <gabriel.volpe@etermax.com>
2014-09-02 12:32:02 -0300

    Se chequea posible error de amazon cuando no existe el archivo.

--- app/controllers/ImageController.scala
+++ app/controllers/ImageController.scala
@@ -3,6 +3,7 @@ package controllers
 import play.api.mvc.{ Action, Controller }
 import aws.AWSFileService
 import aws.S3FileHelper
+import com.amazonaws.services.s3.model.AmazonS3Exception
 
 object ImageController extends Controller {
 
@@ -35,13 +36,18 @@ object ImageController extends Controller {
     }
   }
 
-  def queryS3(name: String) = Action {
-    val s3obj = AWSFileService.query(name)
-    val file = S3FileHelper.convert(s3obj.getObjectContent())
-    val source = scala.io.Source.fromFile(file)(scala.io.Codec.ISO8859)
-    val byteArray = source.map(_.toByte).toArray
-    source.close()
-    Ok(byteArray).as(s3obj.getObjectMetadata().getContentType())
+  def queryS3(name: String) = Action { implicit request =>
+    try {
+      val s3obj = AWSFileService.query(name)
+      val file = S3FileHelper.convert(s3obj.getObjectContent())
+      val source = scala.io.Source.fromFile(file)(scala.io.Codec.ISO8859)
+      val byteArray = source.map(_.toByte).toArray
+      source.close()
+      Ok(byteArray).as(s3obj.getObjectMetadata().getContentType())
+    } catch {
+      case e: AmazonS3Exception =>
+        Ok("Error: " + e.getMessage())
+    }
   }
 
 }
